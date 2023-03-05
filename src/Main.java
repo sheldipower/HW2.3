4965bf462b52b1a689a4106093f8943d62c0a263
Irina <sheldipower@gmail.com>
2023-03-05 15:45:41 +0500

    2

--- src/Main.java
+++ src/Main.java
@@ -8,63 +8,55 @@ public class Main {
         for (int i = 1; i <= 4; i++) {
             DriverD driverD = new DriverD
                     ("driver №" + i,
-                    true,
-                    5+i);
+                            true,
+                            5 + i);
             Bus bus = new Bus(
                     "bus brand №" + i,
                     "bus model №" + i,
                     4.0,
                     driverD,
                     Size.XS);
-            printInfo(bus);
+
             DriverB driverB = new DriverB
                     ("driver №" + i,
                             true,
-                            5+i);
+                            5 + i);
             Car car = new Car(
                     "car brand №" + i,
                     "car model №" + i,
                     1.6,
                     driverB,
                     Bodytype.TYPE6);
-            printInfo(car);
+
             DriverC driverC = new DriverC
                     ("driver №" + i,
                             true,
-                            5+i);
+                            5 + i);
             Truck truck = new Truck(
                     "truck brand №" + i,
                     "truck model №" + i,
                     4.5,
                     driverC,
                     LoadCapacity.N2);
-            printInfo(truck);
+
             truck.setDiagnosticPassed(false);
             try {
-                (bus, truck, car);
+                (bus, truck, car;)
             } catch (TransportTypeException e) {
                 e.printStackTrace();
                 System.out.println(e.getMessage());
             }
-        }
-
-  public static checkTransport (Transport...transports) throws TransportTypeException {
+        } public static checkTransport(Transport...transports) throws TransportTypeException {
             int count = 0;
             for (Transport transport : transports) {
                 if (transport.passDiagnostics) {
                     count++;
                 }
             }
-            System.out.println("Диагностику прошли"+ count+ "из"+transports.length + "автомобилей");
+            System.out.println("Диагностику прошли" + count + "из" + transports.length + "автомобилей");
         }
+
+
+
     }
-
-
-
-    public static void printInfo(Transport <?> transport) {
-        System.out.println(" водитель " + transport.getDriver().getName()+ " управляет автомобилем "  + transport.getBrand() + transport.getModel() +" и будет участвовать в заезде ");
-    }
-
-
-
 }
--- src/transport/TransportTypeException.java
+++ src/transport/TransportTypeException.java
@@ -1,10 +1,22 @@
 package transport;
 
-public class TransportTypeException extends  RuntimeException {
-    public TransportTypeException () {
-
+public class TransportTypeException extends Exception {
+    public TransportTypeException() {
     }
+
     public TransportTypeException(String message) {
-        super (message);
+        super(message);
+    }
+
+    public TransportTypeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TransportTypeException(Throwable cause) {
+        super(cause);
+    }
+
+    public TransportTypeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
